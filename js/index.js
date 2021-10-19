a2c86169de12e8ab91d75c8256de6e0fd6742b9f
Facundo Saracco <facundosaracco@gmail.com>
2021-10-19 07:37:58 +0200

    loops

--- js/index.js
+++ js/index.js
@@ -17,3 +17,28 @@ if (hacker1.length > hacker2.length) {
   }
 
 // Iteration 3: Loops
+
+let hacker1Upper = "";
+for(let i=0; i < hacker1.lenght; i++) {
+  hacker1Upper += hacker1[i].toUpperCase() + " ";
+}
+console.log(hacker1.toUpperCase().split("").join(" "));
+
+let hacker2Inverted = "";
+for(let i = hacker2.lenght - 1; i >= 0; i--){
+  hacker2Inverted += hacker2[i];
+}
+console.log(hacker2.split('').reverse().join(''));
+
+hacker1 = hacker1.toLowerCase();
+hacker2 = hacker2.toLowerCase();
+
+if (hacker1 > hacker2) {
+  console.log("The driver's name goes first.")
+}
+else if (hacker1 < hacker2) {
+  console.log("Yo, the navigator goes first definitely.")
+}
+else {
+  console.log("What?! You both have the same name?")
+}
